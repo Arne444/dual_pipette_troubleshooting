5c8b765cd27e0e3d9ff65b1b7c6489dbe223b160
Arne444 <32069544+Arne444@users.noreply.github.com>
2017-09-21 17:55:00 +0100

    Edit code

--- dual_pipette.py
+++ dual_pipette.py
@@ -5,22 +5,17 @@ from opentrons import robot, containers, instruments
 source_tubes = containers.load('tube-rack-2ml', 'D2', 'tube rack')
 output = containers.load('96-PCR-flat', 'C1', 'output')
 
-p200rack = containers.load('tiprack-10ul-H', 'A1', 'p200-rack')
+p200rack = containers.load('tiprack-200ul', 'A1', 'p200-rack')
 trash = containers.load('trash-box', 'A3')
 
 p200 = instruments.Pipette(
     trash_container=trash,
-    tip_racks=[p200rack],
+    tip_racks=[p200rack.cols('A', 'B')],
     min_volume=20,
     max_volume=200,
     axis="b"
 )
 
-p200.start_at_tip(p20rack['H1'])
-
-p200.distribute(
-	50
-	source_tubes.wells('A1')
-	output.wells('A1' to='D1')
-	new_tip='always'
-)
+for i in range(10):
+	p200.pick_up_tip()
+	p200.return_tip()
