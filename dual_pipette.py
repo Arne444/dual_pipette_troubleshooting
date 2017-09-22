577bfa46aa71ffc51358626673201bfcd5dfa9c7
Arne444 <32069544+Arne444@users.noreply.github.com>
2017-09-22 12:03:41 +0100

    Edit column function

--- dual_pipette.py
+++ dual_pipette.py
@@ -7,13 +7,16 @@ output = containers.load('96-PCR-flat', 'C1', 'output')
 
 p200rack = containers.load('tiprack-200ul', 'A1', 'p200-rack')
 
+column = p200rack.cols('A').wells('8')
+rows = p200rack.rows('1', '8')
+
 p200 = instruments.Pipette(
-    tip_racks=[p200rack.cols('A', to= 'G')],
+    tip_racks=[column],
     min_volume=20,
     max_volume=200,
     axis="b"
 )
 
-for i in range(2):
+for i in range(1):
 	p200.pick_up_tip()
 	p200.return_tip()
