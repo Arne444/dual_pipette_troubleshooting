8699ad2120e3be4a351c742ab248c0c1561de1fb
Arne444 <32069544+Arne444@users.noreply.github.com>
2017-09-22 10:54:53 +0100

    Edit tip_rack code

--- dual_pipette.py
+++ dual_pipette.py
@@ -6,16 +6,14 @@ source_tubes = containers.load('tube-rack-2ml', 'D2', 'tube rack')
 output = containers.load('96-PCR-flat', 'C1', 'output')
 
 p200rack = containers.load('tiprack-200ul', 'A1', 'p200-rack')
-trash = containers.load('trash-box', 'A3')
 
 p200 = instruments.Pipette(
-    trash_container=trash,
-    tip_racks=[p200rack.cols('A', 'B')],
+    tip_racks=[p200rack.cols('A', to= 'G')],
     min_volume=20,
     max_volume=200,
     axis="b"
 )
 
-for i in range(10):
+for i in range(2):
 	p200.pick_up_tip()
 	p200.return_tip()
