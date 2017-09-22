cffa4997a1f626cfb91cb029a2f4e7f27f1cb1f5
Arne444 <32069544+Arne444@users.noreply.github.com>
2017-09-22 11:00:42 +0100

    Edit containers.create

--- dual_pipette.py
+++ dual_pipette.py
@@ -5,7 +5,15 @@ from opentrons import robot, containers, instruments
 source_tubes = containers.load('tube-rack-2ml', 'D2', 'tube rack')
 output = containers.load('96-PCR-flat', 'C1', 'output')
 
-p200rack = containers.load('tiprack-200ul', 'A1', 'p200-rack')
+containers.create(
+'7x12_p200rack',
+grid=(7,12),
+spacing=(9, 9),
+diameter=5,
+depth=60
+)
+
+p200rack = containers.load('7x12_p200rack', 'A1', 'p200-rack')
 
 p200 = instruments.Pipette(
     tip_racks=[p200rack.cols('A', to= 'G')],
@@ -17,3 +25,4 @@ p200 = instruments.Pipette(
 for i in range(2):
 	p200.pick_up_tip()
 	p200.return_tip()
+
