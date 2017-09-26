ea14b15a978578cb03ad5a8850612a4394343309
Arne444 <32069544+Arne444@users.noreply.github.com>
2017-09-26 10:01:10 +0100

    Edit to-rack name WORKS

--- dual_pipette.py
+++ dual_pipette.py
@@ -6,23 +6,23 @@ source_tubes = containers.load('tube-rack-2ml', 'D2', 'tube rack')
 output = containers.load('96-PCR-flat', 'C1', 'output')
 
 containers.create(
-'7x12_p200rack',
+'7x12_tiprack',
 grid=(7,12),
 spacing=(9, 9),
 diameter=5,
 depth=60
 )
 
-p200rack = containers.load('7x12_p200rack', 'A1', 'p200-rack')
+p200rack = containers.load('7x12_tiprack', 'A1', 'p200-rack')
 
 p200 = instruments.Pipette(
-    tip_racks=[p200rack.cols('A', to= 'G')],
+    tip_racks=[p200rack],
     min_volume=20,
     max_volume=200,
     axis="b"
 )
 
-for i in range(2):
+for i in range(10):
 	p200.pick_up_tip()
 	p200.return_tip()
 
